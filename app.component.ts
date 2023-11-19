89b3d0b91a3d26dfaaba8362220a03cf2a9ce73f
رشا أبوالفتوح <140656134+rasha-aboulfotouh@users.noreply.github.com>
2023-11-19 13:52:26 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Rasha5';
+
+  /*customer : any [] = [
+  {  name :"Ayoub", username: "Ghozzi", phone: 0021685129359 }
+  ]
+
+
+  //addcustomer () {
+    this.customer.push ({ name: "new" , username: "new" , phone: 0021630001001 });
+    console.log ("test");
+  }*/
+
+
+
+}
